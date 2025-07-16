7d3eaeaf56af9e8759a331eca864144cf14909f2
Rei Okazawa <reiok0729@gmail.com>
2025-07-17 02:00:14 +0900

    Fix

---
+++ i18n/f0-editor_en.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.ui" line="14"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="109"/>
+        <source>F0 Editor</source>
+        <translation type="unfinished">F0 Editor</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="31"/>
+        <location filename="../mainwindow.ui" line="38"/>
+        <location filename="../mainwindow.ui" line="45"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="111"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="112"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="113"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="66"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="114"/>
+        <source>ファイル</source>
+        <translation type="unfinished">File</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="72"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="115"/>
+        <source>編集</source>
+        <translation type="unfinished">Edit</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="77"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="116"/>
+        <source>表示</source>
+        <translation type="unfinished">View</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.ui" line="87"/>
+        <location filename="../build/arm64-darwin-Debug/ui_mainwindow.h" line="110"/>
+        <source>F0ファイルを開く</source>
+        <translation type="unfinished">Open F0 file</translation>
+    </message>
+</context>
+<context>
+    <name>PitchEditor</name>
+    <message>
+        <location filename="../pitcheditor.cpp" line="40"/>
+        <source>C%1</source>
+        <translation type="unfinished">C%1</translation>
+    </message>
+    <message>
+        <location filename="../pitcheditor.cpp" line="78"/>
+        <source>%1Hzは%2(%3セント). A4=440Hzとした%2は%4Hz.</source>
+        <translation type="unfinished">%1 Hz is %2 (%3 cents). %2 is %4 Hz when A4 is set to 440 Hz.</translation>
+    </message>
+</context>
+</TS>
